20da2903c9d82a38db27b58d2bc015905b3cd416
unknown <chloe.legouas@hotmail.fr>
2024-07-22 01:13:41 +0200

    feat: modification responsive

--- src/components/rating.js
+++ src/components/rating.js
@@ -1,35 +1,31 @@
 import "../sass/main.css";
 import React, { useEffect, useState } from 'react';
-import starG from "../datas/etoileG.svg"
-import starR from "../datas/etoileR.svg"
+import starG from "../datas/etoileG.svg";
+import starR from "../datas/etoileR.svg";
 
+const Ratings = (props) => {
+    const [stars, setStars] = useState([]);
 
-const Raitings = (props) => {
-    const [stars, setStars] = useState ([])
     useEffect(() => {
-        console.log(props.number)
-        const stars_=[]
+        console.log(props.number);
+        const stars_ = [];
         for (let i = 1; i <= 5; i++) {
-
             if (i <= props.number) {
-              stars_.push( <img alt="etoile rouge" src={starR}/>)
-                
+                stars_.push(<img alt="etoile rouge" src={starR} key={i} />);
             } else {
-                stars_.push(<img alt="etoile grise" src={starG}/>)
-                
+                stars_.push(<img alt="etoile grise" src={starG} key={i} />);
             }
         }
-        setStars(stars_)
-    },[]);
+        setStars(stars_);
+    }, [props.number]);
 
-    
     return (
-        <div className="starBox">{stars.map((star,i)=>(
-            <div className="starContent" key={i}> {star} </div>
-        ))}</div>)
+        <div className="starBox">
+            {stars.map((star, i) => (
+                <div className="starContent" key={i}> {star} </div>
+            ))}
+        </div>
+    );
 }
 
-
-
-export default Raitings;
-    
+export default Ratings;
